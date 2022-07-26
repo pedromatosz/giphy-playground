d08e8a68b819e2c4ee261e084b6a5da33375f544
Pedro de Matos <pmatos@questrade.com>
2022-07-26 11:04:35 -0300

    installing necessary packages

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { Home } from './pages/Home'
 import { Favorites } from './pages/Favorites'
+import './styles/reset.scss'
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/favorites" element={<Favorites />} />
