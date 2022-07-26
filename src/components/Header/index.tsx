fbe2066e6b1613fb86a6c0e66f7a2e5d24867c41
Pedro de Matos <pmatos@questrade.com>
2022-07-26 13:11:34 -0300

    feat: finishing header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import S from './style.module.scss'
+import { Link } from 'react-router-dom'
+
+export const Header: React.FC = () => {
+  return (
+    <nav className={S.home__header}>
+      <div className={S.header__logo}>
+        <p className={S.first_word}> GIPHY </p> <p className={S.second_word}> PLAYGROUND</p>
+      </div>
+
+      <div className={S.header__favorite}>
+        <svg className={S.favorite_icon} fill="#DB2A80" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+          />
+        </svg>
+
+        <Link className={S.favorite_link} to="/favorites">
+          My Favorites
+        </Link>
+      </div>
+    </nav>
+  )
+}
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
 import { IHomePageProps } from './types'
+import S from './style.module.scss'
+import { Header } from '../../components/Header'
 
 export const Home: React.FC<IHomePageProps> = () => {
-  return <div>Home</div>
+  return (
+    <div className={S.home}>
+      <Header />
+      <div>
+        <div className={S.search}>
+          <svg className={S.search_icon} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={1}>
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8 16l2.879-2.879m0 0a3 3 0 104.243-4.242 3 3 0 00-4.243 4.242zM21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+            s
+          </svg>
+          <input className={S.search_field} type="text" placeholder="Search gifs" />
+        </div>
+      </div>
+    </div>
+  )
 }
